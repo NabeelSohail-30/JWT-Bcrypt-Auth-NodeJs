dffb2bb56152d6dc5229d4884ae92177d2ca0611
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-26 19:34:14 +0500

    db uri changed

--- server/db.mjs
+++ server/db.mjs
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = "mongodb+srv://dbuser:dbpassword@cluster0.9ha3mra.mongodb.net/?retryWrites=true&w=majority";
+const uri = 'mongodb+srv://NabeelSohail:<password>@cluster0.lidnkc6.mongodb.net/';
 const client = new MongoClient(uri);
 
 export async function connectToDatabase() {
