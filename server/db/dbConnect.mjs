64942ecbc52792c49558d8688b5a68605b9830a7
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-27 10:10:49 +0500

    env variable typo

--- server/db/dbConnect.mjs
+++ server/db/dbConnect.mjs
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
