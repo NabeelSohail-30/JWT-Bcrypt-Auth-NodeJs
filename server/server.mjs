a9e1224ed4815f8f0df54d0e5ac337e3d56a7afa
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-27 09:04:55 +0500

    basic server template

--- server/server.mjs
+++ server/server.mjs
@@ -2,7 +2,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import jwt from 'jsonwebtoken';
 import cookieParser from 'cookie-parser'
 
 dotenv.config();
@@ -21,28 +20,6 @@ app.get('/', (req, res) => {
     });
 })
 
-app.use('/auth', authRouter);
-
-app.use((req, res, next) => {
-    const token = req.cookies.token;
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
-
-    jwt.verify(token, process.env.SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ message: 'Unauthorized' });
-        }
-        req.user = decoded;
-        next();
-    });
-})
-
-// Close the database connection when the server is stopped
-process.on('SIGINT', async () => {
-    await closeDatabaseConnection();
-    process.exit(0);
-});
 
 app.listen(port, async () => {
     console.log(`Server is listening on port http://localhost:${port}`);
