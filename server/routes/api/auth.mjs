133a88fefce938e22163b2fcd9118049ab979089
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-28 18:01:36 +0500

    working on api error

--- server/routes/api/auth.mjs
+++ server/routes/api/auth.mjs
@@ -11,9 +11,9 @@ router.post("/signup", async (req, res) => {
     try {
         const { firstName, lastName, email, password } = req.body;
 
-        if (!firstName || !lastName || !email || !password) {
-            return res.status(400).json({ message: "All fields are required" });
-        }
+        // if (!firstName || !lastName || !email || !password) {
+        //     return res.status(400).json({ message: "All fields are required" });
+        // }
 
         const existingUser = await User.findOne({ email: email.toLowerCase() });
         if (existingUser) {
